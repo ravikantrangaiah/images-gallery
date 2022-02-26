70e670b5bd2925ef78a90a08caf3b13f36c160c1
Ravi K R <ravikant.rangaiah@gmail.com>
2022-02-26 17:49:54 +0530

    added welcome message to home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,26 +4,28 @@ import React from 'react';
 import Header from './components/Header';
 import Search from './components/Search';
 import ImageCard from './components/ImageCard';
-import {Row, Col, Container} from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
+import Welcome from './components/Welcome';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
-
-  const [ word, setWord ] = useState("");
+  const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
         setImages([{ ...data, title: word }, ...images]);
       })
-      .catch((err) =>{
+      .catch((err) => {
         console.log(err);
       });
-      setWord('');
+    setWord('');
   };
 
   const handleDeleteImage = (id) => {
@@ -35,16 +37,20 @@ const App = () => {
       <Header title="Images Gallery"></Header>
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
       <Container className="mt-4">
-        <Row xs={1} md={2} lg={3}>
-          { images.map((image, i) => (
-            <Col className="pb-3" key={i}>
-              <ImageCard image={image} deleteImage={handleDeleteImage} />
-            </Col>
-          ))}
-        </Row>
+        {images.length ? (
+          <Row xs={1} md={2} lg={3}>
+            {images.map((image, i) => (
+              <Col className="pb-3" key={i}>
+                <ImageCard image={image} deleteImage={handleDeleteImage} />
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <Welcome />
+        )}
       </Container>
     </div>
   );
-}
+};
 
 export default App;
