eb3a689d72f214004b72582ff2d4760a0c1a2d64
Ravi K R <ravikant.rangaiah@gmail.com>
2022-02-26 18:40:41 +0530

    eslint and prettier setup

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,18 +2,17 @@ import React from 'react';
 import { Container, Navbar } from 'react-bootstrap';
 
 const navbarStyle = {
-    backgroundColor: 'lightblue'
-  };
-  
-  const Header = (props) => {
-    return (
-        <Navbar style={navbarStyle} variant="light">
-            <Container>
-                <Navbar.Brand href="/">{ props.title }</Navbar.Brand>
-            </Container>
-        </Navbar>
-    
-    )
+  backgroundColor: 'lightblue',
+};
+
+const Header = (props) => {
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -2,20 +2,18 @@ import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 
 const ImageCard = ({ image, deleteImage }) => {
-  return(
+  return (
     <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src={image.urls.small} />
-    <Card.Body>
+      <Card.Img variant="top" src={image.urls.small} />
+      <Card.Body>
         <Card.Title>{image.title.toUpperCase()}</Card.Title>
-        <Card.Text>
-            {image.description || image.alt_description}
-        </Card.Text>
+        <Card.Text>{image.description || image.alt_description}</Card.Text>
         <Button variant="primary" onClick={() => deleteImage(image.id)}>
-            Delete
+          Delete
         </Button>
-    </Card.Body>
+      </Card.Body>
     </Card>
-  )
+  );
 };
 
 export default ImageCard;
